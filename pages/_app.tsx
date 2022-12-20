6fe2e379dcbe22cf209429c7793cf59fcc9b9e92
Adriana Eka Prayudha <radenadriep@gmail.com>
2022-12-21 00:06:47 +0700

    Moving ThemeContextProvider to their own folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 
-import ThemeContextProvider from '@/contexts/ThemeContextProvider'
+import ThemeContextProvider from '@/store/ThemeContextProvider'
 
 import 'inter-ui/inter.css'
 import '../styles/globals.css'
